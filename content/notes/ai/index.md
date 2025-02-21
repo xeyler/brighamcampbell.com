b3ffed8f0cef4d3d63d18fbbc6d4f68aa9360335
Brigham Campbell <me@brighamcampbell.com>
2025-02-21 09:27:56 -0700

    Begin note on AI

---
+++ content/notes/ai/index.md
@@ -0,0 +1,31 @@
++++
+title = 'Outsourcing Muse'
+date = 2025-02-20T17:38:01-07:00
+draft = true
++++
+
+* I spend a lot of time thinking about solutions to problems
+
+In the true spirit of computer science, I spend a lot of time thinking about problems and solutions. Most of the time, I'm just stumped. Unfortunately, I was never really a programming prodigy. I didn't start programming when I was 5. I've never "dreamt in code". In most science, engineering, and math contexts, I'm slower than my peers.
+
+I've learned not to let these facts bother me. After all, if you've solved every problem you've ever tried to solve, maybe you should be working on harder problems. Time is the only fundamental scarcity. The general consensus appears to be that the scarcity of time is good reason to be paranoid about how you spend your time, fending off FOMO. I assert the opposite: The simple act of _spending time_ is just as important and expressive as what you might do _during that time_.
+
+For a long period of time about a decade ago, I considered how to generate mazes of any size programmatically. I remember thinking about possible solutions for days. I tried a couple ideas in Java and never quite got the results I wanted. This problem haunted me... until I simply moved on. It bested me.
+
+* AI makes the process a lot easier
+
+
+
+* easier != better
+    * AI frequently gives wrong answers
+    * Using AI as a crutch is parasocial
+    * Many laws regarding AI training data sets, copyright, and privacy are yet to be written
+        * https://haveibeentrained.com/
+        * If my personal information lands in a training dataset, how do I get it redacted?
+    * AI training and usage contributes to carbon emissions
+* We can all solve problems
+
+A decade after musing about maze generation algorithms, I finally implemented one in Python. [The code](maze.py) isn't pretty, but it was written entirely by me, in an effort to answer a question I had.
+
+
+[![maze generator output](mazer.png)](maze.py)
---
+++ content/notes/ai/maze.py
@@ -0,0 +1,78 @@
+import turtle
+from itertools import product
+from random import random
+
+width = 25
+height = 25
+
+v_domain = range(width)
+v_range = range(height)
+
+# Use prim's algorithm to visit every node in the grid:
+#   * this approach creates two separate trees
+#   * each tree contains a non-zero number of edges
+#   * together, the two trees cover every node
+#   * the edge at which the trees meet is the path through the maze
+
+unvisited_nodes = list(product(v_domain, v_range))
+edges = dict()
+cheapest_cost = dict()
+cheapest_edge = dict()
+selected_paths = list()
+
+for y in range(height):
+    for x in range(width - 1):
+        weight = -1 if y == 0 or y == height - 1 else random()
+        edges[((x, y), (x + 1, y))] = weight
+        edges[((x + 1, y), (x, y))] = weight
+
+for y in range(height - 1):
+    for x in range(width):
+        # "continue" to avoid creating edges at the start and end of the maze
+        if (x, y) == (0, 0):
+            continue
+        if (x, y) == (width - 1, height - 2):
+            continue
+        weight = -1 if x == 0 or x == width - 1 else random()
+        edges[((x, y), (x, y + 1))] = weight
+        edges[((x, y + 1), (x, y))] = weight
+
+for node in unvisited_nodes:
+    if ((0, 0), node) in edges:
+        cheapest_cost[node] = edges[((0, 0), node)]
+        cheapest_edge[node] = ((0, 0), node)
+    elif ((width - 1, height - 1), node) in edges:
+        cheapest_cost[node] = edges[((width - 1, height - 1), node)]
+        cheapest_edge[node] = ((width - 1, height - 1), node)
+    else:
+        cheapest_cost[node] = 1
+
+unvisited_nodes.remove((0, 0))
+unvisited_nodes.remove((width - 1, height - 1))
+
+turtle.setup(width = 500, height = 500)
+turtle.setworldcoordinates(-1, -1, width, height)
+turtle.hideturtle()
+turtle.delay(5000 / (width * height))
+
+# Visit one new node each iteration
+while unvisited_nodes:
+    lowest_cost = 1
+    selected_edge = None
+    for node in unvisited_nodes:
+        if cheapest_cost[node] < lowest_cost:
+            lowest_cost = cheapest_cost[node]
+            selected_edge = cheapest_edge[node]
+    unvisited_nodes.remove(selected_edge[1])
+    selected_paths.append(selected_edge)
+    connected_edges = {k:v for (k,v) in edges.items() if selected_edge[1] in k}
+    for edge in connected_edges:
+        if edges[edge] < cheapest_cost[edge[1]]:
+            cheapest_cost[edge[1]] = edges[edge]
+            cheapest_edge[edge[1]] = edge
+    turtle.penup()
+    turtle.teleport(*selected_edge[0])
+    turtle.pendown()
+    turtle.goto(*selected_edge[1])
+    
+turtle.mainloop()
